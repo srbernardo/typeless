a5cd48c1a0e8ab16c492c1031f82bedaa2846e03
sabrina amaral <sabrinamaraldev@gmail.com>
2023-08-12 20:20:11 -0300

    add edit and update actions, refact :find_expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,10 +1,10 @@
 class ExpensesController < ApplicationController
+  before_action :find_expense, only: %i[show edit update]
   def index
     @expenses = Expense.all
   end
 
   def show
-    @expense = Expense.find(params[:id])
   end
 
   def new
@@ -14,15 +14,25 @@ class ExpensesController < ApplicationController
   def create
     @expense = Expense.new(expense_params)
     @expense.user = current_user
-    @errors = @expense.errors
 
     respond_to do |format|
       if @expense.save
         format.html { redirect_to root_path, notice: "Expense was successfully created." }
-        @expense.save
       else
-        # debugger
         format.html { render "new", status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @expense.update(expense_params)
+        format.html { redirect_to root_path, notice: "Expense was successfully updated." }
+      else
+        format.html { render "edit", status: :unprocessable_entity }
       end
     end
   end
@@ -31,5 +41,9 @@ class ExpensesController < ApplicationController
 
   def expense_params
     params.require(:expense).permit(:date, :place, :description, :quantity, :unity, :value, :category, :payment_type)
+  end
+
+  def find_expense
+    @expense = Expense.find(params[:id])
   end
 end
