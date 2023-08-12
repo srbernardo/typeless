11104c0a733ee8cc8a1dd79e1196f8aa50781e15
Henrique Valeri <henrique_daud@hotmail.com>
2023-08-12 13:55:52 -0300

    actions de new e create

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,2 +1,28 @@
 class IncomesController < ApplicationController
+
+  def new
+    @income = Income.new
+    @user = current_user
+  end
+
+  def create
+    @income = Income.new(income_params)
+    @user = current_user
+    @income.user = @user
+
+    respond_to do |format|
+      if @income.save
+        format.html { redirect_to new_income_path, notice: "Income was successfully added." }
+        @income.save
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:date, :title, :description, :quantity, :value)
+  end
 end
