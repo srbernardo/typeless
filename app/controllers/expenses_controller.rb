deb5f677173f59bf7d6de100f1573e465e6306d4
sabrina amaral <sabrinamaraldev@gmail.com>
2023-08-12 16:53:39 -0300

    add create and new to expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,27 @@
 class ExpensesController < ApplicationController
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user = current_user
+    @errors = @expense.errors
+
+    respond_to do |format|
+      if @expense.save
+        format.html { redirect_to root_path, notice: "Expense was successfully created." }
+        @expense.save
+      else
+        # debugger
+        format.html { render "new", status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:date, :place, :description, :quantity, :unity, :value, :category, :payment_type)
+  end
 end
