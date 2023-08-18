617ea4829815e3328beaaccf2268e8c32654e4a0
bernardo brotelli <b.brotelle@gmail.com>
2023-08-17 21:24:03 -0300

    Helper do veryfi

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,18 @@ require 'json'
 require "httparty"
 
 module ApplicationHelper
-  def ocrVeryfi(urlPhoto)
+
+  def ocr_veryfi(url_photo)
     url = "https://api.veryfi.com/api/v8/partner/documents"
     headers = {
       "Content-Type" => 'application/json',
       "Accept" => 'application/json',
       "CLIENT-ID" => ENV['CLIENT_ID_VERYFI'],
-      "AUTHORIZATION" => "apikey #{ENV['API_KEY_VERYFI']}"
+      "AUTHORIZATION" => "apiKey #{ENV['API_KEY_VERYFI']}"
     }
 
     payload = {
-      file_url: urlPhoto
+      file_url: url_photo
     }.to_json
 
     response = HTTParty.post(url, headers: headers, body: payload)
