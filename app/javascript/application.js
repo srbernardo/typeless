38749692bbf134df45620d409cd0030df6856943
sabrina amaral <sabrinamaraldev@gmail.com>
2023-08-26 12:54:08 -0300

    add some js config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
-import {Turbo} from "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "./controllers"
-// import "@popperjs/core"
+import "@popperjs/core"
 import "bootstrap"
 console.log('teste');
