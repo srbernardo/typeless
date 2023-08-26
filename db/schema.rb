1b05af994da6e1df4fd1a6bcb57f09c08f78639c
sabrina amaral <sabrinamaraldev@gmail.com>
2023-08-26 13:08:27 -0300

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_234449) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.jsonb "ocr_hash"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
