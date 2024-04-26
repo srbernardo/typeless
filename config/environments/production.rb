d924f4d6e92e07c7e24d8caf465840d2e9963e6b
bernardo-brotelli <b.brotelle@gmail.com>
2024-04-26 11:16:31 -0300

    Setup deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "typeless-production.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -23,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
