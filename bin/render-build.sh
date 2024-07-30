1f465f76c043db280d34270a7e8d862a41ab430c
bernardo brotelli <b.brotelle@gmail.com>
2024-07-30 11:17:35 -0300

    Add bundle exec rake db:reset

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rake db:reset
 bundle exec rails generate devise:install
 bundle exec rake db:migrate
 bundle exec rake db:seed
